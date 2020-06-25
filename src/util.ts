82146cf4303c9b5137cd96acf5981309ba3d2d99
Matthew Peveler <matt.peveler@gmail.com>
2020-06-25 18:11:38 -0400

    add util function that contains list of valid signals
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- src/util.ts
+++ src/util.ts
@@ -4,5 +4,49 @@
 export const COMMAND_SLEEP = 25;
 
 export async function sleep(milliseconds?: number): Promise<NodeJS.Timeout> {
-  return new Promise((resolve) => { setTimeout(resolve, milliseconds || COMMAND_SLEEP); });
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds || COMMAND_SLEEP);
+  });
+}
+
+export function validSignal(signal: string): boolean {
+  return [
+    "SIGABRT",
+    "SIGALRM",
+    "SIGBUS",
+    "SIGCHLD",
+    "SIGCONT",
+    "SIGFPE",
+    "SIGHUP",
+    "SIGILL",
+    "SIGINT",
+    "SIGIO",
+    "SIGIOT",
+    "SIGKILL",
+    "SIGPIPE",
+    "SIGPOLL",
+    "SIGPROF",
+    "SIGPWR",
+    "SIGQUIT",
+    "SIGSEGV",
+    "SIGSTKFLT",
+    "SIGSTOP",
+    "SIGSYS",
+    "SIGTERM",
+    "SIGTRAP",
+    "SIGTSTP",
+    "SIGTTIN",
+    "SIGTTOU",
+    "SIGUNUSED",
+    "SIGURG",
+    "SIGUSR1",
+    "SIGUSR2",
+    "SIGVTALRM",
+    "SIGWINCH",
+    "SIGXCPU",
+    "SIGXFSZ",
+    "SIGBREAK",
+    "SIGLOST",
+    "SIGINFO"
+  ].includes(signal);
 }
